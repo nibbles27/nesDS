2ed10e102e1d95cd336b55dbef0a2ec38d7566e6
nibbles27 <145184789+nibbles27@users.noreply.github.com>
2023-09-16 06:58:23 -0700

    Add files via upload

--- arm9/source/mappers/map5.s
+++ arm9/source/mappers/map5.s
@@ -5,31 +5,32 @@
 	#include "6502mac.h"
 @---------------------------------------------------------------------------------
 	.global mapper5init
+	.global NES_XRAM
 	counter = mapperdata+0
 	enable = mapperdata+1
 	prgsize = mapperdata+2
 	chrsize = mapperdata+3
-	prgpage0 = mapperdata+4
-	prgpage1 = mapperdata+5
-	prgpage2 = mapperdata+6
-	prgpage3 = mapperdata+7
-	chrpage0 = mapperdata+8
-	chrpage1 = mapperdata+9
-	chrpage2 = mapperdata+10
-	chrpage3 = mapperdata+11
-	chrpage4 = mapperdata+12
-	chrpage5 = mapperdata+13
-	chrpage6 = mapperdata+14
-	chrpage7 = mapperdata+15
-	chrpage8 = mapperdata+16
-	chrpage9 = mapperdata+17
-	chrpage10 = mapperdata+18
-	chrpage11 = mapperdata+19
-	chrbank = mapperdata+20
-	mmc5irqr = mapperdata+21
-	mmc5mul1 = mapperdata+22
-	mmc5mul2 = mapperdata+23
-	m5mirror = mapperdata+24
+	chrbank = mapperdata+4
+	mmc5irqr = mapperdata+5
+	mmc5mul1 = mapperdata+6
+	mmc5mul2 = mapperdata+7
+	m5mirror = mapperdata+8
+	prgpage0 = mapperdata+12
+	prgpage1 = mapperdata+13
+	prgpage2 = mapperdata+14
+	prgpage3 = mapperdata+15
+	chrpage0 = mapperdata+28
+	chrpage1 = mapperdata+32
+	chrpage2 = mapperdata+36
+	chrpage3 = mapperdata+40
+	chrpage4 = mapperdata+44
+	chrpage5 = mapperdata+48
+	chrpage6 = mapperdata+52
+	chrpage7 = mapperdata+56
+	chrpage8 = mapperdata+60
+	chrpage9 = mapperdata+64
+	chrpage10 = mapperdata+68
+	chrpage11 = mapperdata+72
 @---------------------------------------------------------------------------------
 mapper5init:
 @---------------------------------------------------------------------------------
@@ -57,11 +58,13 @@ mapper5init:
 	mov pc,lr
 @---------------------------------------------------------------------------------
 write0:
-@---------------------------------------------------------------------------------
+@----------------------------------监视 char *-----------------------------------------------
 	cmp addy,#0x5000
 	blo IO_W
 	cmp addy,#0x5100
 	blo map5Sound
+	cmp addy,#0x5c00
+	bge mmc5_c00w
 	cmp addy,#0x5200
 	bge mmc5_200
 
@@ -81,6 +84,8 @@ write0:
 	ble _20
 	cmp r2,#0x2b
 	ble _28
+	cmp r2,#0x30
+	beq _30
 	mov pc,lr		@ get out.
 
 _00:
@@ -97,6 +102,8 @@ _05:
 	beq mirror5_1
 	cmp r0,#0
 	beq mirror5_1
+	cmp r0, #0xaa
+	beq mirror_xram_0000
 	cmp r0,#0xE4
 	beq mirror4_
 	eor r1,r0,r0,lsr#4
@@ -164,131 +171,136 @@ _24:
 _25:
 _26:
 _27:
-	mov r1,#0
-	strb_ r1,chrbank
+	ldrb_ r1,chrbank
+	orr r0, r0, r1, lsl#8
 	adrl_ r1,chrpage0
 	sub r2,r2,#0x20
-	strb r0,[r1,r2]
+	add r1, r1, r2, lsl#2
+	str r0,[r1]
 mmc5chra:
 @	mov pc,lr		; get out?
 	ldrb_ r1,chrsize
 	cmp r1,#0x00
 	bne notch0
-	ldrb_ r0,chrpage7
+	ldr_ r0,chrpage7
 @	mov r0,r0,lsr#3
 	b chr01234567_
 notch0:
 	str lr,[sp,#-4]!
 	cmp r1,#0x01
 	bne notch1
-	ldrb_ r0,chrpage3
+	ldr_ r0,chrpage3
 @	mov r0,r0,lsr#2
 	bl chr0123_
 	ldr lr,[sp],#4
-	ldrb_ r0,chrpage7
+	ldr_ r0,chrpage7
 @	mov r0,r0,lsr#2
 @	b chr4567_
 	mov pc,lr		@ get out?
 notch1:
 	cmp r1,#0x02
 	bne notch2
-	ldrb_ r0,chrpage1
+	ldr_ r0,chrpage1
 @	mov r0,r0,lsr#1
 	bl chr01_
-	ldrb_ r0,chrpage3
+	ldr_ r0,chrpage3
 @	mov r0,r0,lsr#1
 	bl chr23_
-	ldrb_ r0,chrpage5
+	ldr_ r0,chrpage5
 @	mov r0,r0,lsr#1
 @	bl chr45_
 	ldr lr,[sp],#4
-	ldrb_ r0,chrpage7
+	ldr_ r0,chrpage7
 @	mov r0,r0,lsr#1
 @	b chr67_
 	mov pc,lr		@ get out?
 notch2:
-	ldrb_ r0,chrpage0
+	ldr_ r0,chrpage0
 	bl chr0_
-	ldrb_ r0,chrpage1
+	ldr_ r0,chrpage1
 	bl chr1_
-	ldrb_ r0,chrpage2
+	ldr_ r0,chrpage2
 	bl chr2_
-	ldrb_ r0,chrpage3
+	ldr_ r0,chrpage3
 	bl chr3_
-	ldrb_ r0,chrpage4
+	ldr_ r0,chrpage4
 @	bl chr4_
-	ldrb_ r0,chrpage5
+	ldr_ r0,chrpage5
 @	bl chr5_
-	ldrb_ r0,chrpage6
+	ldr_ r0,chrpage6
 @	bl chr6_
 	ldr lr,[sp],#4
-	ldrb_ r0,chrpage7
+	ldr_ r0,chrpage7
 @	b chr7_
 	mov pc,lr		@ get out?
-
+_30:
+	and r0, r0, #3
+	strb_ r0, chrbank
+	mov pc, lr
 _28:				@ For background.
 _29:
 _2a:
 _2b:
-	mov r1,#1
-	strb_ r1,chrbank
+	ldrb_ r1,chrbank
+	orr r0, r0, r1, lsl#8
 	adrl_ r1,chrpage0
 	sub r2,r2,#0x20
-	strb r0,[r1,r2]
+	add r1, r1, r2, lsl#2
+	str r0,[r1]
 mmc5chrb:
 @	mov pc,lr		; get out?
 	ldrb_ r1,chrsize
 	cmp r1,#0x00
 	bne notchb0
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 	mov r0,r0,lsr#3
 	b chr01234567_
 notchb0:
 	str lr,[sp,#-4]!
 	cmp r1,#0x01
 	bne notchb1
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 @	mov r0,r0,lsr#2
 @	bl chr0123_
 	ldr lr,[sp],#4
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 	mov r0,r0,lsr#2
 	b chr4567_
 	mov pc,lr
 notchb1:
 	cmp r1,#0x02
 	bne notchb2
-	ldrb_ r0,chrpage9
+	ldr_ r0,chrpage9
 @	mov r0,r0,lsr#1
 @	bl chr01_
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 @	mov r0,r0,lsr#1
 @	bl chr23_
-	ldrb_ r0,chrpage9
+	ldr_ r0,chrpage9
 	mov r0,r0,lsr#1
 	bl chr45_
 	ldr lr,[sp],#4
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 	mov r0,r0,lsr#1
 	b chr67_
 	mov pc,lr
 notchb2:
-	ldrb_ r0,chrpage8
+	ldr_ r0,chrpage8
 @	bl chr0_
-	ldrb_ r0,chrpage9
+	ldr_ r0,chrpage9
 @	bl chr1_
-	ldrb_ r0,chrpage10
+	ldr_ r0,chrpage10
 @	bl chr2_
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 @	bl chr3_
-	ldrb_ r0,chrpage8
+	ldr_ r0,chrpage8
 	bl chr4_
-	ldrb_ r0,chrpage9
+	ldr_ r0,chrpage9
 	bl chr5_
-	ldrb_ r0,chrpage10
+	ldr_ r0,chrpage10
 	bl chr6_
 	ldr lr,[sp],#4
-	ldrb_ r0,chrpage11
+	ldr_ r0,chrpage11
 	b chr7_
 
 map5Sound:
@@ -315,11 +327,29 @@ setEnIrq:
 	and r0,r0,#0x80
 	strb_ r0,enable
 	mov pc,lr
+@---------------------------------------------------------------------------------
+mmc5_c00w:
+	@dup write, no need
+	@ldr r1, =NES_XRAM - 0x4000
+	@strb r0, [r1, addy]
+
+	/* update BG */
+	ldr r1, =NES_XRAM + 0x1C00
+	ldr r2, =NDS_BG+0x2000
+	b writeBG
+@---------------------------------------------------------------------------------
+@ never reach here, the NES_XRAM is read from memmap_tbl
+mmc5_c00r:
+	ldr r1, =NES_XRAM - 0x4000
+	ldrb r0, [r1, addy]
+	mov pc, lr
 
 @---------------------------------------------------------------------------------
 mmc5_r:		@5204,5205,5206
 	cmp addy,#0x5200
 	blo IO_R
+	cmp addy,#0x5C00
+	bge mmc5_c00r
 	and r2,addy,#0xff
 	cmp r2,#0x04
 	beq MMC5IRQR
@@ -341,13 +371,13 @@ MMC5IRQR:
 
 MMC5MulA:
 	ldrb_ r1,mmc5mul1
-	ldrb_ r2,mmc5mul1
+	ldrb_ r2,mmc5mul2
 	mul r0,r1,r2
 	and r0,r0,#0xff
 	mov pc,lr
 MMC5MulB:
 	ldrb_ r1,mmc5mul1
-	ldrb_ r2,mmc5mul1
+	ldrb_ r2,mmc5mul2
 	mul r0,r1,r2
 	mov r0,r0,lsr#8
 	mov pc,lr
